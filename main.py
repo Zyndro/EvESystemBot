af12483428187f49d3cabedb9781170013c77867
Sindromo <lis.mateusz@ymail.com>
2022-04-05 21:18:37 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import requests
+import discord
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+wpierdol = ["R1O-GN", "LXQ2-T", "4-HWWF", "T5ZI-S","Y-MPWL","PR-8CA","J-ZYSZ","D-PNP9","G-M4GK","UGR-J2","MJ-5F9","Q-5211"]
+jita = ["Jita"]
+
+def systemsEVE(sysname):
+    sysname = sysname
+    connectionlist = []
+    try:
+        for destynacjax in sysname:
+            url = 'https://www.eve-scout.com/api/wormholes?systemSearch=' + destynacjax + '&method=shortest&limit=1000&offset=0&sort=jumps&order=asc'
+            r = requests.get(url, headers={'accept': 'application/json'})
+            connectionlisttemp = []
+            for x in r.json():
+                jmp = x['jumps']
+                name = x['destinationSolarSystem']['name']
+                sig = x['signatureId']
+                reg = x['destinationSolarSystem']['region']['name']
+                eol = x['wormholeEol']
+                if 0 < jmp < 14:
+                    connectionlisttemp.append(f' {destynacjax} \n sygnatura: {sig} \n ile: {jmp} \n wlotowy: {name} \n region: {reg} \n EoL: {eol}')
+                if jmp > 14:
+                    daleko = (f'{destynacjax} za daleko albo nima')
+            if not connectionlisttemp:
+                connectionlist.append(daleko)
+            if connectionlisttemp:
+                for x in connectionlisttemp:
+                    connectionlist.append(x)
+
+        return connectionlist
+    except Exception as e:
+        return e
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$systemy'):
+        for x in systemsEVE(wpierdol):
+            await message.channel.send("```yaml\n"+x+"```")
+
+    if message.content.startswith('$jita'):
+        for x in systemsEVE(jita):
+            await message.channel.send("```yaml\n"+x+"```")
+
+client.run(TOKEN)
---
+++ readme.md
@@ -0,0 +1,3 @@
+Absolute garbage, add a .env file to the script root directory with your discord bot token and run(dont).
+
+commands: $jita $systemy
