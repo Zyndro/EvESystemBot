8f4013547c3b0c08f81bcaf589f0ff8732498018
Sindromo <lis.mateusz@ymail.com>
2022-04-06 18:42:46 +0200

    $systemy

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import discord
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
-wpierdol = ["R1O-GN", "LXQ2-T", "4-HWWF", "T5ZI-S","Y-MPWL","PR-8CA","J-ZYSZ","D-PNP9","G-M4GK","UGR-J2","MJ-5F9","Q-5211"]
+wpierdol = ["R1O-GN", "LXQ2-T", "4-HWWF", "T5ZI-S", "Y-MPWL", "PR-8CA", "J-ZYSZ", "D-PNP9", "G-M4GK", "UGR-J2",
+            "MJ-5F9", "Q-5211"]
 jita = ["Jita"]
 
+
 def systemsEVE(sysname):
-    sysname = sysname
     connectionlist = []
     try:
         for destynacjax in sysname:
@@ -24,11 +24,10 @@ def systemsEVE(sysname):
                 reg = x['destinationSolarSystem']['region']['name']
                 eol = x['wormholeEol']
                 if 0 < jmp < 14:
-                    connectionlisttemp.append(f' {destynacjax} \n sygnatura: {sig} \n ile: {jmp} \n wlotowy: {name} \n region: {reg} \n EoL: {eol}')
-                if jmp > 14:
-                    daleko = (f'{destynacjax} za daleko albo nima')
+                    connectionlisttemp.append(
+                        f' {destynacjax} \n sygnatura: {sig} \n ile: {jmp} \n wlotowy: {name} \n region: {reg} \n EoL: {eol}')
             if not connectionlisttemp:
-                connectionlist.append(daleko)
+                connectionlist.append((f'{destynacjax} za daleko albo nima'))
             if connectionlisttemp:
                 for x in connectionlisttemp:
                     connectionlist.append(x)
@@ -37,11 +36,14 @@ def systemsEVE(sysname):
     except Exception as e:
         return e
 
+
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -51,14 +53,15 @@ async def on_message(message):
     if message.content.startswith('$systemy'):
         await message.channel.send("Szukam...")
         for x in systemsEVE(wpierdol):
-            await message.channel.send("```yaml\n"+x+"```")
+            await message.channel.send("```yaml\n" + x + "```")
+        await message.channel.send("Znalazłem!")
 
     if message.content.startswith('$jita'):
         for x in systemsEVE(jita):
-            await message.channel.send("```yaml\n"+x+"```")
+            await message.channel.send("```yaml\n" + x + "```")
 
     if message.content.startswith('$help'):
-            await message.channel.send("```yaml\n"+"Dostępne komendy: \n $jita \n $systemy"+"```")
+        await message.channel.send("```yaml\n" + "Komendy: \n $jita \n $systemy" + "```")
 
 
 client.run(TOKEN)
